9813ddf0f2b39aaef21e4b85beeb7fce6c822561
re <121850050+reized@users.noreply.github.com>
2024-09-29 21:19:09 +0700

    menginstall tailwind css

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import "./index.css";
 import { createRoot } from 'react-dom/client';
  
-const element = <h1>Ini adalah Page Registrasi</h1>;
+const element = <h1 className='text-xl text-blue-700'>Ini adalah Page Registrasi</h1>;
  
 const root = createRoot(document.getElementById('root'));
 root.render(element);
