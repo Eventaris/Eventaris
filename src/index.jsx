c1d748d5c534b1527fa25f4032d26e3d72003672
Lyan_Nandyan <lyannandyan@gmail.com>
2024-10-16 22:29:05 +0700

    mengecilkan font

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./index.css";
 import { createRoot } from 'react-dom/client';
  
-const element = <h1 className='text-9xl text-blue-700'>Ini adalah Page Registrasi</h1>;
+const element = <h1 className='text-4xl text-blue-700'>Ini adalah Page Registrasi</h1>;
  
 const root = createRoot(document.getElementById('root'));
 root.render(element);
