4d9bd6f56d2fa61bf939bba91a3f7ed79181a4d8
Lyan_Nandyan <lyannandyan@gmail.com>
2024-10-19 19:35:18 +0700

    mengubah border color

--- src/components/RegisForm.jsx
+++ src/components/RegisForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import InputField from "./InputField";
-const style="mt-1 block w-96 px-1 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500`"
+const style="mt-1 block w-96 px-1 py-1 bg-white border border-[#0E738C] rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#0E738C] focus:ring-1 focus:ring-[#0E738C]"
 class RegisForm extends React.Component{
     constructor(props){
         super(props)
@@ -60,12 +60,12 @@ class RegisForm extends React.Component{
                 </InputField>
                 
                 <InputField
-                style={style+"disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"} 
+                style={style+"invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"} 
                 type={"email"} 
                 name={"Email"} 
                 value={this.state.email} 
                 onchange={this.onEmailChangeEventHandler}>
-
+                {/*Kok warnanya gak bisa berubah ijo fokusnya, malah biru*/}
                 </InputField>
              
                 <InputField 
