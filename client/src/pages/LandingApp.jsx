89418a2033298855b56d265a1336cd4e7ceaf361
re <121850050+reized@users.noreply.github.com>
2024-10-25 14:04:40 +0700

    membuat step, about, dan footer section

--- client/src/pages/LandingApp.jsx
+++ client/src/pages/LandingApp.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import CtaBtn from "../components/CtaBtn";
 import CategoryCard from "../components/CategoryCard";
+import StepCard from "../components/StepCard";
+import Footer from "../components/Footer";
 
 const LandingApp = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <div>
-            <Navbar />
+            <Navbar isLoggedIn={isLoggedIn} />
+            {/* Hero Section */}
             <div
                 className="bg-blue-700 h-screen bg-cover bg-center pt-16 px-24 flex items-center"
                 style={{ backgroundImage: "url('src/assets/Hero Banner.png')" }}
@@ -26,6 +31,8 @@ const LandingApp = () => {
                     <CtaBtn />
                 </div>
             </div>
+
+            {/* Category Section */}
             <div className="h-96 flex flex-col justify-center items-center gap-8">
                 <div className="text-center">
                     <h1 className="text-green2 text-4xl font-bold">
@@ -43,6 +50,50 @@ const LandingApp = () => {
                         ))}
                 </div>
             </div>
+
+            {/* Step Section */}
+            <div className="px-24 h-screen bg-green3 flex items-center justify-between">
+                <div className="flex flex-col">
+                    <h1 className="text-green2 text-4xl font-bold">
+                        How it works?
+                    </h1>
+                    <p className="text-[#667085]">
+                        Just take some easy steps to get wat you need
+                    </p>
+                </div>
+                <div className="flex gap-8">
+                    <StepCard />
+                    <StepCard />
+                    <StepCard />
+                </div>
+            </div>
+
+            {/* Abot Section */}
+            <div className="px-24 h-screen flex justify-between items-center">
+                <div className="w-5/12 flex justify-start">
+                    <img src="src/assets/about-illustration.png" alt="" />
+                </div>
+                <div className="w-1/2 flex flex-col gap-4">
+                    <h1 className="text-green2 text-4xl font-bold">About</h1>
+                    <p className="text-[#667085]">
+                        We are a comprehensive platform dedicated to providing
+                        everything you need for events or organizational
+                        requirements. From weddings and parties to corporate
+                        gatherings and organizational needs, we serve as a
+                        bridge connecting clients with trusted partners offering
+                        high-quality rental items. Our mission is to simplify
+                        the process of finding the right products, ensuring that
+                        every detail of your event or project is handled with
+                        care and professionalism. Let us help you create
+                        seamless, successful, and unforgettable experiences.
+                    </p>
+                </div>
+            </div>
+            
+            {/* Footer */}
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
