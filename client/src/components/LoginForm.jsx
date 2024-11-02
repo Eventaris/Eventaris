e02ee7df809f61951c14532b13dac076de2db5a7
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-02 18:54:44 +0700

    menambahkan loading screen di login page

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import InputField from "./InputField";
 import LoginOrRegist from "../components/LoginOrRegist";
+import Loading from "./Loading";
 const style="mt-1 block w-96 px-1 py-1 bg-white border border-[#0E738C] rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-[#0E738C] focus:ring-1 focus:ring-[#0E738C]"
 class LoginForm extends React.Component{
     constructor(props){
@@ -8,11 +9,13 @@ class LoginForm extends React.Component{
 
         this.state = {
             username: '',
-            pass: ''
+            pass: '',
+            isLoading: false
         }
 
         this.onUsernameChangeEventHandler = this.onUsernameChangeEventHandler.bind(this);
         this.onPassChangeEventHandler = this.onPassChangeEventHandler.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
     }
 
     onUsernameChangeEventHandler(event){
@@ -30,14 +33,19 @@ class LoginForm extends React.Component{
         })
     }
 
+    onSubmitHandler(event){
+        event.preventDefault()
+        this.setState({isLoading: true})
+    }
+
     render(){
         return(
             <div className="Login-form">
-                <form action="submit">
+                <form onSubmit={this.onSubmitHandler}>
                 <InputField
-                style={style}
-                type={"text"}
-                name={"Username"}
+                style={style + "invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"}
+                type={"email"}
+                name={"Email"}
                 value={this.state.usernamne}
                 onchange={this.onUsernameChangeEventHandler}>
 
@@ -55,6 +63,11 @@ class LoginForm extends React.Component{
                 <button className="hover:bg-green2 hover:brightness-110 rounded-md bg-[#07A0A8] w-96 py-[0.5rem] text-white text-center mt-10 font-inter text-[13px]" type="submit">Log In</button>
                 </form>
                 <LoginOrRegist text="Don’t have an account?  " sub="Create account" target="/registrasi"/>
+
+                {this.state.isLoading && (
+                <Loading/>
+                )}
+
             </div>
         )
     }
