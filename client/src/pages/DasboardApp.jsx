0a3420018ab57382763149dff3fa23fbd25ad3ee
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-09 18:21:50 +0700

    mengecek auth, dan menerima data nama dan email, akun yang login

--- client/src/pages/DasboardApp.jsx
+++ client/src/pages/DasboardApp.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 const DasboardApp = () => {
-    const userData = localStorage.getItem('user');
-    const user = JSON.parse(userData);
-    return(
-        <div>
-            <h1>Hello {user.nama} This Is Main Page After Login</h1>
-        </div>
-    )
+    const [user, setUser] = useState(null);
+    useEffect(()=>{
+    fetch("http://localhost:5000/dasboard", {
+        method: "GET",
+        credentials: 'include',
+    })
+        .then((response) => {
+            if (!response.ok) {
+                return response.json().then((errorData) => {
+                    throw new Error(errorData.message || "Terjadi kesalahan");
+                });
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setUser(data.user);
+        })
+        .catch((error) => {
+            //alert(`Error: ${error.message}`);
+            sessionStorage.setItem("errorMessage", error.message);
+            window.location.href = "/login";
+        });
+    },[]);
+    if (user) {
+        return (
+            <div>
+                <h1>Hello {user.nama} This Is Main Page After Login</h1>
+            </div>
+        )
+    }
 }
 
 export default DasboardApp
