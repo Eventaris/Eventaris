23ea0f5b85c17027c49c4a899653be8ad6f0e8b9
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-11 00:43:21 +0700

    membuat komponen kategori bar

--- client/src/components/CategoryBar.jsx
+++ client/src/components/CategoryBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const CategoryBar = ({cate}) =>{
-    return(
-        <div>
-
+const CategoryBar = ({ cate }) => {
+    return (
+        <div className="text-xs absolute top-0 right-0 bg-green5 px-4 py-2 text-white mt-3 mr-3">
+            {cate}
         </div>
     )
 }
