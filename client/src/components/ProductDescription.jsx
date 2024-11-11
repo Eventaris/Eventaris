5bb78d9ffa79dd0a3d7ff241f03c74808298a96f
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-11 18:46:52 +0700

    menambah harga di deskripsi card

--- client/src/components/ProductDescription.jsx
+++ client/src/components/ProductDescription.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-const ProductDescription = ({ title, desc, link }) => {
+const ProductDescription = ({ title, desc, harga }) => {
     return (
         <div className="px-6 py-4 mb-auto">
-            <a href={link}
+            <p
                 className="font-medium text-lg  hover:text-green1 transition duration-300 ease-in-out inline-block mb-2">{title}
-            </a>
+            </p>
             <p className="text-gray-500 text-sm">
                 {desc}
+            </p>
+            <p className="font-bold text-right">
+                Rp. {harga}
             </p>
         </div>
     )
