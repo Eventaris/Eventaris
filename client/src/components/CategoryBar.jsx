32c550f306cc8bf0a5aa63026f6f7bf1eb744466
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-11 18:46:29 +0700

    menambah rounded di bar category

--- client/src/components/CategoryBar.jsx
+++ client/src/components/CategoryBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CategoryBar = ({ cate }) => {
     return (
-        <div className="text-xs absolute top-0 right-0 bg-green5 px-4 py-2 text-white mt-3 mr-3">
+        <div className="rounded-lg text-xs absolute top-0 right-0 bg-green5 px-4 py-2 text-white mt-3 mr-3">
             {cate}
         </div>
     )
